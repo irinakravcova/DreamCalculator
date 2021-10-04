436e367aece84454e31c24563993b7e3827a6760
Irina Kravcova <irina.kravcova@gmail.com>
2021-10-04 21:08:18 +0300

    Adding SQL Table Creation

---
+++ sql/dreamcalculator_ddl.sql
@@ -0,0 +1,11 @@
+create table project (
+familyName varchar (55) not null,
+dreamSum float  not null,
+income float not null,
+billsTotal float not null,
+percent float ,
+Result1 varchar (300),
+Result2 varchar (300),
+Result3 varchar (300),
+primary key (familyName)
+);
